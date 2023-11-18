29eae0731ff6dcfb2b946bb455014373060582a2
Андрей Медведев <Tyuiu.MedvedevA@outlook.com>
2023-11-18 18:47:05 +0500

    Add project files.

---
+++ Tyuiu.MedvedevA.Sprint5.Task0.V12.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.MedvedevA.Sprint5.Task0.V12.Lib;
+using System.IO;
+
+namespace Tyuiu.MedvedevA.Sprint5.Task0.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\Users\D1BOO.\source\repos\Tyuiu.MedvedevA.Sprint5\Tyuiu.MedvedevA.Sprint5.Task0.V12\bin\Debug\OutPutFileTask0.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool tyta = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, tyta);
+        }
+    }
+}
---
+++ Tyuiu.MedvedevA.Sprint5.Task0.V12/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MedvedevA.Sprint5.Task0.V12.Lib;
+
+namespace Tyuiu.MedvedevA.Sprint5.Task0.V12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            
+
+            int x = 3;
+
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"X = {x}");
+
+            string res = ds.SaveToFileTextData(x);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"Файл: {res}\nСоздан!");
+            Console.ReadKey();
+        }
+    }
+}
